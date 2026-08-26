aee656a844666cf251102a94562b94cd27cf9fe4
qiukeng007 <qiukeng007@163.com>
2026-08-26 14:01:28 +0200

    备份 v1.0.11：统一操作记录写入商品描述

---
+++ lib/models/restock_prefill_data.dart
@@ -0,0 +1,20 @@
+/// Data passed from QueryPage to RestockPage via HomePage
+class RestockPrefillData {
+  final String barcode;
+  final String supplier;
+  final String productName;
+  final String specification;
+  final double? buyPrice;
+  final double? sellPrice;
+  final String? imageUrl;
+
+  const RestockPrefillData({
+    required this.barcode,
+    this.supplier = '',
+    this.productName = '',
+    this.specification = '',
+    this.buyPrice,
+    this.sellPrice,
+    this.imageUrl,
+  });
+}
